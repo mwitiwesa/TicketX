d3b69a92f904ed9b26d15ff317457ca668c9e08d
MwitiWesa-coke <wesamwiti01@gmail.com>
2026-04-01 00:28:11 +0300

    Created a debug log to view why superuser can't login

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.http import HttpResponse
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -61,3 +65,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     @property
     def is_admin(self):
         return self.role == 'ADMIN'
+    
--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
     path('register/', views.register, name='register'),
+    path('debug-login/', views.debug_login, name='debug_login'),
 ]
