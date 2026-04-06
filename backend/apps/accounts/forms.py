6dfe9338ce7ef3ca0820b71f6b840b3009b95004
MwitiWesa-coke <wesamwiti01@gmail.com>
2026-04-06 06:59:07 +0300

    Added CUstom Authentication Form to fix login

--- backend/apps/accounts/forms.py
+++ backend/apps/accounts/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .models import User
 
+# Form for Login (uses Email instead of Username)
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.EmailField(
         label="Email Address",
@@ -15,3 +16,9 @@ class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('email', 'password')
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2')
