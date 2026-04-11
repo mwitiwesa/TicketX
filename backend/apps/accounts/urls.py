3ae7d37570ccc0e5d0966fb66443a8c2266a5f28
MwitiWesa-coke <wesamwiti01@gmail.com>
2026-04-11 11:35:57 +0300

    Forgot button added to  login page

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -12,5 +12,31 @@ urlpatterns = [
     ), name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.register, name='register'),
-    path('promote-admin/', views.promote_admin, name='promote_admin'),
+    #path('promote-admin/', views.promote_admin, name='promote_admin'),
+    # ====================== PASSWORD RESET ======================
+    path('password_reset/', 
+         auth_views.PasswordResetView.as_view(
+             template_name='accounts/password_reset.html',
+             email_template_name='accounts/password_reset_email.html',
+             subject_template_name='accounts/password_reset_subject.txt',
+         ), 
+         name='password_reset'),
+
+    path('password_reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='accounts/password_reset_done.html'
+         ), 
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='accounts/password_reset_confirm.html'
+         ), 
+         name='password_reset_confirm'),
+
+    path('reset/done/', 
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='accounts/password_reset_complete.html'
+         ), 
+         name='password_reset_complete'),
 ]
